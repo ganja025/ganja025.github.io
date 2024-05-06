900fb990b8a65f6a6c4ce17b610f423702c7436f
firstatack <joaquinbolson80@gmail.com>
2024-05-06 18:58:22 -0300

    subida1

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1715032626',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
